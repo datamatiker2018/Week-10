e9904ab0ceac14f7583f7f89c0dbd2577679b311
Rune A. Nielsen <runeanielsen@live.dk>
2018-03-07 16:24:54 +0100

    Added reverse name solution

---
+++ Reverse name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string name = "Rune";
+            string reversedName = "";
+
+            foreach (var character in name)
+            {
+                reversedName = character + reversedName;
+            }
+
+            Console.WriteLine(reversedName);
+            Console.ReadKey();
+        }
+    }
+}
