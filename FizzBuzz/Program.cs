46d1e4b2f911aa8be41c6db251fed94cfaaa6823
Rune A. Nielsen <runeanielsen@live.dk>
2018-03-07 16:13:15 +0100

    Added fizzbuzz solution

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fizz)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
