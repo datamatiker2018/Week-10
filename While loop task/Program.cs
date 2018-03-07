74c4410e6e61ddcbe7bf571feb17e3d113118fd5
Rune A. Nielsen <runeanielsen@live.dk>
2018-03-07 16:19:59 +0100

    Added while loop solution

---
+++ While loop task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhileExample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int months = 24;
+            int annieSavings = 0;
+
+            while (months != 0)
+            {
+                annieSavings += 300;
+                months--;
+            }
+
+            Console.WriteLine(annieSavings);
+        }
+    }
+}
